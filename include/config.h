988ae8a380def91aee253d54b6f963d3857f5937
SmartPolarBear <CleverCoolBear@outlook.com>
2022-02-23 22:46:31 +0800

    feat(main): add argument parser
    chore(libraries): add fmt library

---
+++ include/config.h
@@ -0,0 +1,27 @@
+//
+// Created by cleve on 2/23/2022.
+//
+#pragma once
+
+// Check the libc++ version
+#include <version>
+
+#ifndef _LIBCPP_VERSION
+#error "chclang must be link against libc++"
+#elif _LIBCPP_VERSION < 13000
+#error "chclang must be link against libc++-13 and above
+#endif
+
+#include <string_view>
+
+namespace chclang::config
+{
+
+struct predefined_configurations
+{
+	static inline constexpr std::string_view PROGRAM_NAME = "chclang";
+	static inline constexpr  std::string_view  PROGRAM_VERSION = "0.0.1";
+
+	static inline constexpr size_t MAX_FILES=256;
+};
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
-#include <iostream>
+#include "config.h"
 
-int main()
+#include <iostream>
+#include <string>
+
+#include <argparse/argparse.hpp>
+
+using namespace chclang::config;
+
+using namespace argparse;
+
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello, World!" << std::endl;
+	ArgumentParser program{ string{ predefined_configurations::PROGRAM_NAME },
+							string{ predefined_configurations::PROGRAM_VERSION }};
+
+	program.add_argument("files")
+			.help("The files to be compiled")
+			.nargs(predefined_configurations::MAX_FILES)
+			.required();
+
+	program.add_argument("-o", "--output")
+			.nargs(1)
+			.required()
+			.help("Specify the output file.");
+
+	try
+	{
+		program.parse_args(argc, argv);
+	}
+	catch (const std::runtime_error& err)
+	{
+		std::cerr << err.what() << std::endl;
+		std::cerr << program;
+		std::exit(1);
+	}
+
 	return 0;
 }
